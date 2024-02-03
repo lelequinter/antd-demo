0a5901a53242397cad45e4fe58f1cf520133e02e
Leyder Quintero <desarrollador9@wedevel.co>
2024-02-03 11:44:21 -0500

    mouse leave and mouse enter in sider bar

--- src/app/components/test.tsx
+++ src/app/components/test.tsx
@@ -1,75 +1,96 @@
+"use client";
 import React from "react";
+
+import Sider from "antd/es/layout/Sider";
 import { Layout, Menu, MenuProps } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import {
-    AppstoreOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    ShopOutlined,
-    TeamOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-import Sider from "antd/es/layout/Sider";
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
 
-const items: MenuProps['items'] = [
-    UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    AppstoreOutlined,
-    TeamOutlined,
-    ShopOutlined,
-  ].map((icon, index) => ({
-    key: String(index + 1),
-    icon: React.createElement(icon),
-    label: `nav ${index + 1}`,
-  }));
+const items: MenuProps["items"] = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
 
 const TestComponent = () => {
+  const mouseEnter = () => {
+    console.log("mouse enter");
+  };
 
-
+  const mouseLeave = () => {
+    console.log("mouse leave");
+  };
 
   return (
     <Layout hasSider>
       <Sider
-        // onmo
-        style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }}
+        trigger={null}
+        collapsible
+        collapsed={true}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
       >
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
       </Sider>
-      <Layout style={{ marginLeft: 70 }}>
-        <Header style={{ padding: 0, background: '#fff' }} />
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+      <Layout style={{ marginLeft: 80 }}>
+        <Header style={{ padding: 0, background: "#fff" }} />
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
           <div
             style={{
               padding: 24,
-              textAlign: 'center',
-              background: '#fff',
+              textAlign: "center",
+              background: "#fff",
               borderRadius: 4,
             }}
           >
-            <p style={{color: "black"}}>long content</p>
+            <p style={{ color: "black" }}>long content</p>
             {
               // indicates very long content
               Array.from({ length: 100 }, (_, index) => (
-                <p key={index} style={{color: "black"}}>
-                  {index % 20 === 0 && index ? 'more' : '...'}
+                <p key={index} style={{ color: "black" }}>
+                  {index % 20 === 0 && index ? "more" : "..."}
                   <br />
                 </p>
               ))
             }
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
+        <Footer style={{ textAlign: "center" }}>
           Ant Design ©{new Date().getFullYear()} Created by Ant UED
         </Footer>
       </Layout>
     </Layout>
-
   );
 };
 
